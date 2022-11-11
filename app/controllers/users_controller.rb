438e39e85c4e21e828c6d9d7bc70a44a5e2a7211
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-11-11 17:59:48 -0500

    define full CRUD actions in the Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,30 @@
 class UsersController < ApplicationController
+    
     def index
         users = User.all
         render json: users
     end
 
-    # to build full CRUD functionality 
     def show 
-        #
+        user = User.find_by(id: params[:id])
+        render json: user
     end
 
     def create
-        # 
+        user = User.create!(params)
+        render json: user, status: :created
     end
 
     def update
-        #
+        user = User.find_by(id: params[:id])
+        user.update(params)
+        render json: user
     end
 
     def destroy
-        #
+        user = User.find_by(id: params[:id])
+        user.destroy
+        head :no_content 
     end
 
 
