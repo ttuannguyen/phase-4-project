c518ea36bce2ace42478bb120539d75a53d4fd85
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-10-21 13:56:41 -0400

    fix the names of the paths for imports in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignUp from './components/SignUp';
-import Home from './components/Home';
-import LogIn from './components/LogIn';
-import Navbar from './components/Navbar';
-import UserSecretSpots from './components/UserSecretSpots';
+import SignUp from './Components/SignUp';
+import Home from './Components/Home';
+import LogIn from './Components/LogIn';
+import Navbar from './Components/Navbar';
+import UserSecretSpots from './Components/UserSecretSpots';
 import { UserProvider } from './context/user';
-import VisitEditForm from './components/VisitEditForm';
-import SecretSpotDetails from './components/SecretSpotDetails';
-import Header from './components/Header';
+import VisitEditForm from './Components/VisitEditForm';
+import SecretSpotDetails from './Components/SecretSpotDetails';
+// import Header from './components/Header';
 
 const App = () => {
 
