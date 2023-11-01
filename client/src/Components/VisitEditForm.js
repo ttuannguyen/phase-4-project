031c88c867aee9e4037e3727899bcac7ab67e0a8
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-10-31 22:37:14 -0400

    update visit edit form to include the date update option and pre-populate the form with data in state

--- client/src/Components/VisitEditForm.js
+++ client/src/Components/VisitEditForm.js
@@ -1,13 +1,14 @@
 import React, { useState, useContext } from 'react';
 import { UserContext } from '../context/user';
 import { useParams, useNavigate } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
 
 const VisitEditForm = () => {
 
     const { user, updateVisit, loggedIn, toggle, setToggle } = useContext(UserContext);
     const params = useParams(); 
-    const [note, setNote] = useState('');
-    const navigate = useNavigate('');
+    // const [note, setNote] = useState('');
+
     const [visitFound, setVisitFound] = useState({});
 
     if (!visitFound.id && user.id) {
@@ -15,16 +16,33 @@ const VisitEditForm = () => {
         setVisitFound(vf)
     }
 
+    const [formData, setFormData] = useState({
+        date: visitFound.date,
+        note: visitFound.note   
+      });
+
+    const handleChange = (e) => {
+        setFormData(formData => {
+            return {...formData, [e.target.name]:e.target.value}
+        })
+    };  
+
+
+    const navigate = useNavigate('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        setFormData({
+            date:'',
+            note:''
+        })
+
         fetch(`/users/${user.id}/visits/${params.id}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                note: note
-            })
+            body: JSON.stringify(formData)
         })
         .then(res => res.json())
         .then(data => {
@@ -36,16 +54,33 @@ const VisitEditForm = () => {
 
     if (loggedIn) {
         return (
-            <div className='visit-edit-div'>
-                <form onSubmit={handleSubmit}>
-                    <h4>{visitFound.secret_spot}</h4>
-                    <p>Date: {visitFound.date}</p>
-                    <br/>
-                    <label>Note:</label><br/>
-                    <textarea placeholder={visitFound.note} type="text" name='note' value={note} onChange={e => setNote(e.target.value)} /><br/>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+
+            <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Date</Form.Label>
+              <Form.Control type='text' name='date' value={formData.date} onChange={handleChange} defaultValue={visitFound.date} />
+            </Form.Group>
+  
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Note</Form.Label>
+              <Form.Control type="text" name='note' value={formData.note} onChange={handleChange} defaultValue={visitFound.note} />
+            </Form.Group>
+  
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+
+            // <div className='visit-edit-div'>
+            //     <form onSubmit={handleSubmit}>
+            //         <h4>{visitFound.secret_spot}</h4>
+            //         <label>Note:</label><br/>
+            //         <textarea placeholder={visitFound.note} type="text" name='note' value={note} onChange={e => setNote(e.target.value)} /><br/>
+            //         <label>Note:</label><br/>
+            //         <textarea placeholder={visitFound.note} type="text" name='note' value={note} onChange={e => setNote(e.target.value)} /><br/>
+            //         <button type="submit">Submit</button>
+            //     </form>
+            // </div>
         )
     } else {
         return (<h4>Please login or create an Account</h4>)
